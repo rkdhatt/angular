ff1a51647efb0ab459691d6b50ce9d1d230ea7fa
= <=>
2018-03-11 15:00:04 -0600

    class binding example

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,20 +4,9 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-        <button class="btn btn-primary">Save</button>
+        <button class="btn btn-primary" [class.active]="isActive">Save</button>
     `
 })
 export class CoursesComponent {
-    title = "List of courses";
-    courses;
-
-    constructor(service: CoursesService){
-        this.courses = service.getCourses();
-    }
-    // Logic for calling an HTTP service within component
-    // Issues with this approach:
-    // 1. Logic will tightly couple the component with the HTTP endpoint. Not good for unit tests.
-    // 2. courses may be part of another page (ex: dashboard) -- causes repeating in multiple places
-    // 3. Component should not include any other logic besides presentation related logic.
-    // Solution: created separate Service class.
+    isActive = true; // if false, then the 'active' class will not show in button.
 }
