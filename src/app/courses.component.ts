ab4caf7dd77d5d4d2ffbba74aaa1e024d8ff7612
= <=>
2018-03-11 15:08:55 -0600

    example of style binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-        <button class="btn btn-primary" [class.active]="isActive">Save</button>
+        <button [style.backgroundColor]="isActive ? 'blue' : 'white'"]>Save</button>
     `
 })
 export class CoursesComponent {
