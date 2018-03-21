f8bfcf907281b8aa01b473dfdf60555f3cf4f903
= <=>
2018-03-20 21:06:38 -0600

    pipes example

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,15 +4,19 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
     template: `
-        <input [(ngModel)]="email" (keyup.enter)="onKeyUp()"/>
-    ` // the event will be called only when keup.enter is recognized.
-    // [()] is a two-way binding syntax in angular.
-    // Better than this approach: <input [value]="email" (keyup.enter)="email = $event.target.value; onKeyUp()"/>
+        {{ course.title | uppercase | lowercase }} <br/>
+        {{ course.rating | number:'1.2-2' }} <br/>
+        {{ course.students | number }} <br/>
+        {{ course.price | currency:'AUD' :true }} <br/>
+        {{ course.releaseDate | date:'shortDate' }} <br/>
+    `
 })
 export class CoursesComponent {
-    email = "me@example.com";
-    onKeyUp(){
-        console.log(this.email); // get the value entered in input (can also do $event.target.value).
-        // Template variables can help simplify code.
-    }       
+    course = {
+        title: "Angular Course",
+        rating: 4.9745,
+        students: 30123,
+        price: 190.95,
+        releaseDate: new Date(2016, 3, 1)
+    }      
 }
