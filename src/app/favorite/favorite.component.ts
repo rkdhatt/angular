3d89aa686327d85c8a2e2713fca42378b8b9ed46
= <=>
2018-03-29 20:37:53 -0600

    aliasing output property for easier refacotring

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
   @Input('isFavorite') isSelected = false;
-  @Output() change = new EventEmitter();
+  @Output('change') click = new EventEmitter();
 
   constructor() { }
 
@@ -16,7 +16,7 @@ export class FavoriteComponent implements OnInit {
 
   onClick(){
     this.isSelected = !this.isSelected;
-    this.change.emit( {newValue: this.isSelected} ); //notifying others that something has happened.
+    this.click.emit( {newValue: this.isSelected} ); //notifying others that something has happened.
   }
 }
 
