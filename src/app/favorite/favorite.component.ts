7559967a6be9d40448b2cc4d4ded6e6a49939fb3
= <=>
2018-04-01 17:51:00 -0600

    example of how to use view encapsulation for shadow DOM

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,18 +1,14 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
-export class FavoriteComponent implements OnInit {
+export class FavoriteComponent {
   @Input('isFavorite') isSelected = false;
   @Output('change') click = new EventEmitter();
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 
   onClick(){
     this.isSelected = !this.isSelected;
