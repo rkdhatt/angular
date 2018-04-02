45296306d4eb5b4a7a8bfd1a2ce9dafa8fef8763
= <=>
2018-04-01 19:43:21 -0600

    ngFor and change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,5 +13,14 @@ export class AppComponent {
     {id: 2, name: 'course2'},
     {id: 3, name: 'course3'}
   ];
+
+  onAdd(){
+    this.courses.push({id: 4, name: 'course4'});
+  }
+
+  onRemove(course){
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
 }
  
